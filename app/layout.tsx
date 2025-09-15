438f004b6ddc8aca4a9020db70d988cab0ab06af
Granted07 <anjishnu.040302007@gmail.com>
2025-09-15 14:52:02 +0530

    update: theme change

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,7 @@ export default function RootLayout({
             >
             <ThemeProvider
             attribute={`class`}
-            defaultTheme="light"
-            enableSystem
+            defaultTheme="dark"
             disableTransitionOnChange
             >
                 <Nav></Nav>
